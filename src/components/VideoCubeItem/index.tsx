3402f512e7f6e31ffefb98ffbe7114f4e0f14020
iamsydali <iamsydali@gmail.com>
2022-12-06 16:58:19 +0500

    Fixed Video playing issue.

--- src/components/VideoCubeItem/index.tsx
+++ src/components/VideoCubeItem/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {
   StyleSheet,
   View,
@@ -7,9 +7,10 @@ import {
 } from 'react-native';
 import Video from 'react-native-video';
 
-const { width: screenW, height: screenH } = Dimensions.get('window');
+const {width: screenW, height: screenH} = Dimensions.get('window');
 
-const VIDEO_URL = 'https://res.cloudinary.com/facetally/video/upload/du_10,e_loop,eo_10,f_gif,q_auto:low,so_0,vs_60/v1658270939/wzrad4yreyux2ekg12bj.gif';
+const VIDEO_URL =
+  'https://res.cloudinary.com/facetally/video/upload/du_10,e_loop,eo_10,f_gif,q_auto:low,so_0,vs_60/v1658270939/wzrad4yreyux2ekg12bj.gif';
 
 const VideoCubeItem = ({
   source,
@@ -33,18 +34,19 @@ const VideoCubeItem = ({
 
   return (
     <View style={styles.main}>
-      <TouchableWithoutFeedback
+      <Video
+        ref={(ref: undefined) => (videoPlayerRef.current = ref)}
+        source={{uri: source || VIDEO_URL}}
         style={styles.video}
+        repeat
+        onError={console.log}
+        paused={isVideoPaused}
+        controls
+        resizeMode="cover"
+      />
+      <TouchableWithoutFeedback
         onPress={() => setIsVideoPaused(!isVideoPaused)}>
-        <Video
-          ref={(ref: undefined) => (videoPlayerRef.current = ref)}
-          source={{ uri: source || VIDEO_URL }}
-          style={styles.video}
-          repeat
-          onError={console.log}
-          paused={isVideoPaused}
-          fullScreen
-        />
+        <View style={styles.videoView} />
       </TouchableWithoutFeedback>
     </View>
   );
@@ -54,7 +56,16 @@ export default VideoCubeItem;
 
 const styles = StyleSheet.create({
   main: {
+    justifyContent: 'center',
+    alignItems: 'center',
     flex: 1,
+  },
+  videoView: {
+    position: 'absolute',
+    zIndex: 1,
+    top: 0,
+    height: screenH * 0.8,
+    width: screenW,
   },
   video: {
     height: screenH * 0.8,
